56b4bb4a179cfd08b20e2dbb6fda3b5e08582d9b
ilopeza <ilopezarzuaga@gmail.com>
2020-06-28 18:21:17 -0300

    First commit

---
+++ src/main/java/com/musicinfofinder/musicinfofinderservice/MusicInfoFinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musicinfofinder.musicinfofinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicInfoFinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicInfoFinderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicinfofinder/musicinfofinderservice/MusicInfoFinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicinfofinder.musicinfofinderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicInfoFinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

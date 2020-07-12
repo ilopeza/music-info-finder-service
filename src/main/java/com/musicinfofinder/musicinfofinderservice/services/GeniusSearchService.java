f98c1cbfeb009e0f11b9245c4bf91052192a9641
ilopeza <ilopezarzuaga@gmail.com>
2020-07-12 17:15:02 -0300

    Add new api to search within the genius response hits.

--- src/main/java/com/musicinfofinder/musicinfofinderservice/services/GeniusSearchService.java
+++ src/main/java/com/musicinfofinder/musicinfofinderservice/services/GeniusSearchService.java
@@ -4,11 +4,24 @@ import com.musicinfofinder.musicinfofinderservice.exceptions.BadRequestException
 import com.musicinfofinder.musicinfofinderservice.models.requests.search.GeniusSearchLyricsRequest;
 import com.musicinfofinder.musicinfofinderservice.models.requests.search.SearchLyricsRequest;
 import com.musicinfofinder.musicinfofinderservice.models.response.BaseGeniusResponse;
-import org.apache.commons.lang.StringUtils;
+import com.musicinfofinder.musicinfofinderservice.models.response.search.Hit;
+import com.musicinfofinder.musicinfofinderservice.models.response.search.HitType;
+import com.musicinfofinder.musicinfofinderservice.models.response.search.Result;
+import com.musicinfofinder.musicinfofinderservice.models.response.search.SearchResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+import static java.util.Optional.empty;
+import static org.apache.commons.lang.StringUtils.equalsIgnoreCase;
+import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 @Service
 public class GeniusSearchService implements SearchService {
@@ -20,9 +33,23 @@ public class GeniusSearchService implements SearchService {
 
     @Override
     public BaseGeniusResponse findLyrics(SearchLyricsRequest request) {
-        if (StringUtils.isBlank(request.getAlbumName())
-                && StringUtils.isBlank(request.getArtistName())
-                && StringUtils.isBlank(request.getTrackName())) {
+        return searchInGenius(request);
+    }
+
+    @Override
+    public Optional<Result> findResult(SearchLyricsRequest request) {
+        BaseGeniusResponse geniusResponse = searchInGenius(request);
+        if (isNull(geniusResponse)) {
+            return empty();
+        }
+        Optional<Result> resultInGeniusResponse = findResultInGeniusResponse(request, geniusResponse);
+        return resultInGeniusResponse;
+    }
+
+    private BaseGeniusResponse searchInGenius(SearchLyricsRequest request) {
+        if (isBlank(request.getAlbumName())
+                && isBlank(request.getArtistName())
+                && isBlank(request.getTrackName())) {
             //bad request exception?
             throw new BadRequestException("Artist name and track name can not be blank");
         }
@@ -32,8 +59,35 @@ public class GeniusSearchService implements SearchService {
                 .withWebClientBuilder(webClientBuilder)
                 .build();
 
-        BaseGeniusResponse response = geniusSearchLyricsRequest.execute();
-
-        return response;
+        return geniusSearchLyricsRequest.execute();
     }
+
+
+    private Optional<Result> findResultInGeniusResponse(SearchLyricsRequest request, BaseGeniusResponse response) {
+        if (isBlank(request.getArtistName())
+                || isBlank(request.getTrackName())
+                || isBlank(request.getAlbumName())) {
+            throw new BadRequestException("Filters can not be empty");
+        }
+
+        if (isNull(response)) {
+            return empty();
+        }
+        SearchResponse searchResponse = response.getResponse();
+        List<Hit> hits = searchResponse.getHits();
+        if (CollectionUtils.isEmpty(hits)) {
+            return empty();
+        }
+        Optional<Result> optionalResult = hits.stream().filter(hit -> !isNull(hit.getResult()) && HitType.SONG.equals(hit.getType()))
+                .map(Hit::getResult)
+                .filter(result -> equalsIgnoreCase(request.getTrackName(), result.getTitle()) ||
+                        (isNotBlank(result.getFullTitle())
+                                && result.getFullTitle().contains(request.getTrackName())))
+                .filter(result -> !isNull(result.getPrimaryArtist())
+                        && isNotBlank(result.getPrimaryArtist().getName())
+                        && equalsIgnoreCase(result.getPrimaryArtist().getName(), request.getArtistName()))
+                .findFirst();
+        return optionalResult;
+    }
+
 }
